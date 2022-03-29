1aefe72f9744b92f807be57751aa9289d5061fc2
James Delemar <delemar@hotmail.com>
2022-03-28 20:25:46 -0400

    ✨Create React Storybook (#18)
    * ♻️Rename Web project to Angular-Web
    
    * ✨Add React Web application
    
    * ✨React-Web: Add storybook with test story
    
    * 🔧Update GitHub action

---
+++ apps/react-web/src/app/test.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+const TempComponent = () => {
+  return (
+    <>
+      <h2>TempComponent works!</h2>
+      <p>Hosted on GitHub Pages</p>
+    </>
+  );
+};
+
+export default {
+  title: 'Web/Test',
+  component: TempComponent,
+} as ComponentMeta<typeof TempComponent>;
+
+const Template: ComponentStory<typeof TempComponent> = (args) =>
+  <TempComponent />;
+
+export const TestComponent = Template.bind({});
+TestComponent.args = {};
