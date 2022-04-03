eb0612b24fdcc58fd578c1881ed43dc39051dbce
James <delemar@hotmail.com>
2022-04-03 16:25:04 -0400

    ✨Composition: Storybook references multiple storybooks
    Related work items: #17

--- apps/composition-web/.storybook/main.js
+++ apps/composition-web/.storybook/main.js
@@ -11,6 +11,33 @@ module.exports = {
     '../src/app/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [...rootMain.addons],
+
+  refs: (config, { configType }) => {
+    if (configType === 'DEVELOPMENT') {
+      return {
+        angular: {
+          title: 'Angular Storybook (development)',
+          url: 'http://localhost:6007',
+        },
+        react: {
+          title: 'React Storybook (development)',
+          url: 'http://localhost:6008',
+        },
+      };
+    } else {
+      return {
+        angular: {
+          title: 'Angular Storybook',
+          url: 'https://jdelemar.github.io/component-monorepo',
+        },
+        react: {
+          title: 'React Storybook',
+          url: 'https://jdelemar.github.io/storybook-react',
+        },
+      };
+    }
+  },
+
   webpackFinal: async (config, { configType }) => {
     // apply any global webpack configs that might have been specified in .storybook/main.js
     if (rootMain.webpackFinal) {
